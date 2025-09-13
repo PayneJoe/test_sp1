67ce2ab4a94ec91aac4d16a6d49fde713820bdc7
joe <joepayne@163.com>
2025-09-13 09:15:37 +0000

    fix secp256k1 error

--- test_program/src/main.rs
+++ test_program/src/main.rs
@@ -2,21 +2,21 @@
 
 sp1_zkvm::entrypoint!(main);
 
-//  use bitcoin::{
-//      Script,
-//      TapSighashType,
-//      Transaction,
-//      TxOut,
-//      XOnlyPublicKey,
-//      key::Secp256k1,
-//      secp256k1::{Message, VerifyOnly, schnorr::Signature},
-//      sighash::{Prevouts, SighashCache},
-//      taproot::ControlBlock,
-//  };
-//  use sp1_verifier::Groth16Verifier;
-//  use sp1_verifier::hash_public_inputs;
-//  use std::sync::LazyLock;
-// 
-//  pub static SECP: LazyLock<Secp256k1<VerifyOnly>> = LazyLock::new(Secp256k1::verification_only);
+use bitcoin::{
+    Script,
+    TapSighashType,
+    Transaction,
+    TxOut,
+    XOnlyPublicKey,
+    key::Secp256k1,
+    secp256k1::{Message, VerifyOnly, schnorr::Signature},
+    sighash::{Prevouts, SighashCache},
+    taproot::ControlBlock,
+};
+use sp1_verifier::Groth16Verifier;
+use sp1_verifier::hash_public_inputs;
+use std::sync::LazyLock;
+
+pub static SECP: LazyLock<Secp256k1<VerifyOnly>> = LazyLock::new(Secp256k1::verification_only);
 
 pub fn main() {}
